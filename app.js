433aab03a245eaac1d1191cc9b31d2563a6f84ea
Francislene Menezes de Souza <142364644+FranSou23@users.noreply.github.com>
2024-06-03 22:26:32 -0300

    execicio

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function Converter(cep) {
+    fetch("https://cep.awesomeapi.com.br/json/"+cep)
+        .then((response) => response.json())
+        .then((data) => alert(data.address +" "+data.district+" "+ data.city))
+        .then((error) => console.log(error))
+}
+
+document.getElementById("endereco").addEventListener("click",()=>{
+    let cep = document.getElementById("cep").value
+    Converter(cep)
+})
+
